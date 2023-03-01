1a788a34a6986b6f4809ef5ddb7cbeb6f0f8cb80
AartiPundalik <aartipundalik68@gmail.com>
2023-03-01 19:49:43 +0530

    [Aarti] Add : UC2 - Test Case For Last Name

--- TestCaseRegex/UnitTest1.cs
+++ TestCaseRegex/UnitTest1.cs
@@ -19,11 +19,5 @@ public class UnitTest1
         string lastname = regexprogram.ValidateLastName("Pundalik");
         Assert.AreEqual(lastname, "Pundalik");
     }
-    [TestMethod]
-    public void Test3()
-    {
-        string email_id = regexprogram.ValidateEmail("abc.xyz@bl.co.in");
-        Assert.AreEqual(email_id, "abc.xyz@bl.co.in");
-    }
 }
 
